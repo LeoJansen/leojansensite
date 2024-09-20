0955594257cf94585e9ad2d8112e373c79c9a938
Léo Jansen <leo@leoo.app>
2024-09-20 00:41:00 -0300

    Update main.js

--- vite/main.js
+++ vite/main.js
@@ -2,6 +2,16 @@ import * as THREE from 'three';
 
 const canvas = document.getElementById(elementId: 'canvas');
 
-//1. Create the scene
+//1. Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('#F0F0F0');
+
+//2. Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+//3. Object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshBasicMaterial(parameters:{ color: '#468585', emissive: "#468585"});
+const dodecahedron = new THREE.Mesh(geometry, material);
+scene.add(dodecahedron);
