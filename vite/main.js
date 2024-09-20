19099e4941465f4c3bed4d1665a551b335529b93
Léo Jansen <leo@leoo.app>
2024-09-20 00:37:36 -0300

    Updates

---
+++ vite/main.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+const canvas = document.getElementById(elementId: 'canvas');
+
+//1. Create the scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
