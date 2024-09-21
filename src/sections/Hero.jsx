4cdc4b8e45b6fe859aaa5d16f9d29bb99b7c951d
Léo Jansen <leo@leoo.app>
2024-09-20 23:51:33 -0300

    Creates Hero

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,16 @@
+
+
+const Hero = () => {
+  return (
+   <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
+    <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="text-xl sm:text-3xl font-medium text-white text-center font-generalsans">Hi, I am Leo <span className="waving-hand">👋</span> </p>
+        <p className="hero_tag text-gray_gradient ">Building Products & Brands</p>
+
+    </div>
+
+   </section>
+  )
+}
+
+export default Hero
