e45fea56cf272ef87d98fc32608022b8a5724983
Léo Jansen <leo@leoo.app>
2024-09-21 02:23:41 -0300

    Inserts models folder

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,25 +1,32 @@
 import { PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import HackerRoom from "../../components/HackerRoom"
+import { Suspense } from "react"
 
 
 
 
 const Hero = () => {
   return (
-   <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
-    <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+    <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="text-xl sm:text-3xl font-medium text-white text-center font-generalsans">Hi, I am Leo <span className="waving-hand">👋</span> </p>
         <p className="hero_tag text-gray_gradient ">Building Products & Brands</p>
-    </div>
-    <div className="w-full h-full absolute inset-0">
-      <Canvas className="w-full h-full">
-        <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom fallback={null} />
 
-      </Canvas>
 
-    </div>
+          </Suspense>
 
-   </section>
+        </Canvas>
+
+      </div>
+
+    </section>
   )
 }
 
