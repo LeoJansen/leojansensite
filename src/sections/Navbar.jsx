a67011ba6681eeb969a03f230112bfd721138b6b
Léo Jansen <leo@leoo.app>
2024-09-20 22:14:32 -0300

    Update Navbar.jsx

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -6,10 +6,10 @@ const NavItems = () => {
 
     return (
         <ul className="nav-ul">
-            {navLinks.map((item, index) => (
+            {navLinks.map(({name, href}, index) => (
                 <li key={index} className="nav-li">
-                    <a href={item.href} className="nav-li_a">
-                        {item.name}
+                    <a href={href} className="nav-li_a" onClick={()=> {}}>
+                        {name}
                     </a>
                 </li>   
             ))}
@@ -35,11 +35,19 @@ const Navbar = () => {
                     <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle Menu">
                         <img src={ isOpen ? 'assets/close.svg':'assets/menu.svg'} alt="menu" className="w-6 h-6"/>
                     </button>
-                    <nav>
+                    <nav className="sm:flex hidden">
                         <NavItems/>
 
                     </nav>
                 </div>
+            </div>
+            <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+                <nav className="p-5">
+
+                    <NavItems/>
+
+                </nav>
+
             </div>
         </header>
     );
