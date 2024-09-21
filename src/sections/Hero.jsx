cd5475a778697d17d11dc16172079c6c084611ab
Léo Jansen <leo@leoo.app>
2024-09-21 17:44:17 -0300

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,21 +4,53 @@ import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
 import CanvasLoader from "../components/CanvasLoader"
 import HackerRoom from "../components/HackerRoom"
-import { usex } from "leva"
+import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive"
 
 
 
 
 const Hero = () => {
-  const x = usex('Hacker Room', {
+  const isMobile = useMediaQuery({maxWidth: 768});
+  const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+  const x = useControls('HackerRoom', {
     positionX: {
       value: 2.5,
       min: -10,
       max: 10,
-    }
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    scale: {
+      value: 0.07,
+      min: 0.01,
+      max: 0.5,
+    },
+  });
 
-  },
-  );
 
   return (
     <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
@@ -27,13 +59,15 @@ const Hero = () => {
         <p className="hero_tag text-gray_gradient ">Building Products & Brands</p>
       </div>
       <div className="w-full h-full absolute inset-0">
+        <Leva />
+
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
             <HackerRoom
-              scale={[x.positionX, x.positionY, x.positionZ]}
-              position={[0, 0, 0]}
-              rotation={[0, 280, 0]}
+              scale={isMobile ? 0.07 : 0.1}
+              position={isMobile ? [0.5,-3,2.9] : [0.5,-10,2.9]}
+              rotation={[0.4, 3.2, 0.0]}
             />
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
