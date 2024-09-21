d6034dc8cc7b6c88b9d7d67146a6ecc380d0a8bd
Léo Jansen <leo@leoo.app>
2024-09-21 20:09:42 -0300

    Adjusts model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,51 +6,16 @@ import CanvasLoader from "../components/CanvasLoader"
 import HackerRoom from "../components/HackerRoom"
 import { Leva, useControls } from "leva";
 import { useMediaQuery } from "react-responsive"
+import { calculateSizes } from "../constants"
 
 
 
 
 const Hero = () => {
-  const isMobile = useMediaQuery({maxWidth: 768});
-  const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
-  const x = useControls('HackerRoom', {
-    positionX: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    positionY: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    positionZ: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    rotationX: {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    rotationY: {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    rotationZ: {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    scale: {
-      value: 0.07,
-      min: 0.01,
-      max: 0.5,
-    },
-  });
-
+  const isSmall = useMediaQuery({maxWidth: 480});
+  const isMobile = useMediaQuery({minWidth: 480.001, maxWidth: 768});
+  const isTablet = useMediaQuery({minWidth: 768.001, maxWidth: 1024});
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
     <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
@@ -59,15 +24,15 @@ const Hero = () => {
         <p className="hero_tag text-gray_gradient ">Building Products & Brands</p>
       </div>
       <div className="w-full h-full absolute inset-0">
-        <Leva />
+        {/* <Leva /> */}
 
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
             <HackerRoom
-              scale={isMobile ? 0.07 : 0.1}
-              position={isMobile ? [0.5,-3,2.9] : [0.5,-10,2.9]}
-              rotation={[0.4, 3.2, 0.0]}
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[0.2, -Math.PI, 0]}
             />
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
