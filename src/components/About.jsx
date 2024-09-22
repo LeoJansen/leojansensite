5a157ac908f267d0b69f891543df29467a255783
Léo Jansen <leo@leoo.app>
2024-09-22 14:37:58 -0300

    Creates About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,20 @@
 const About = () => {
   return (
     <section className="c-space my-20">
-        <div className="">
+        <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+          <div className="col-span-1 xl:row-span-3">
+            <div className="grid-container">
+              <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
+              <div>
+                <p className="grid-headtext"> Hi, I&apos;m Leo.</p>
+                <p className="grid-subtext">With 4 years of experience, I am a full-stack developer.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-1">
+            
+
+          </div>
 
         </div>
 
