954ab84dd24f45b756a226e346fb4b5b4153ccf9
Léo Jansen <leo@leoo.app>
2024-09-22 00:17:01 -0300

    Updates

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,6 +7,9 @@ import HackerRoom from "../components/HackerRoom"
 import { Leva, useControls } from "leva";
 import { useMediaQuery } from "react-responsive"
 import { calculateSizes } from "../constants"
+import Target from "../components/Target"
+import ReactLogo from "../components/ReactLogo"
+import Cube from "../components/Cube"
 
 
 
@@ -19,9 +22,9 @@ const Hero = () => {
 
   return (
     <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 z-20">
         <p className="text-xl sm:text-3xl font-medium text-white text-center font-generalsans">Hi, I am Leo <span className="waving-hand">👋</span> </p>
-        <p className="hero_tag text-gray_gradient ">Building Products & Brands</p>
+        <p className="hero_tag text-blue-500 text-3xl font-bold  ">Building Products & Brands</p>
       </div>
       <div className="w-full h-full absolute inset-0">
         {/* <Leva /> */}
@@ -34,6 +37,12 @@ const Hero = () => {
               position={sizes.deskPosition}
               rotation={[0.2, -Math.PI, 0]}
             />
+            <group>
+              <Target position={sizes.targetPosition}/>
+              <ReactLogo position={sizes.reactLogoPosition} scale={sizes.reactLogoScale}/>
+              <Cube position={sizes.cubePosition}/>
+            </group>
+
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
 
