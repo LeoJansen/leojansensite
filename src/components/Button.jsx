e928da5f470ace83bcc1501dd889d1203145b1b4
Léo Jansen <leo@leoo.app>
2024-09-22 04:17:55 -0300

    Updates Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 
 
-export const Button = ({children}) => {
+export const Button = ({name, isBeam=false, containerClass  }) => {
   return (
-    <div>{children}</div>
+    <button className={`btn ${containerClass}`  }>
+       { isBeam && ( <span className="relative flex h-3 w-3">
+            <span className="btn-ping"/>
+            <span className="btn-ping_dot"/>
+        </span>)}
+        {name}
+        </button>
   )
 }
 
