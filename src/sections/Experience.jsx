4703257bb167824da336c1f23681e0c344cd3f2c
Léo Jansen <leo@leonardojansen.com>
2024-09-27 15:45:56 -0300

    Update Experience.jsx

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,22 +1,35 @@
 import { Canvas } from "@react-three/fiber"
+import { workExperiences } from "../constants"
 
 
 const Experience = () => {
   return (
     <div className="c-space my-20">
-        <div className="w-full text-white-600">
-          <h3 className="head-text" > My work experience</h3>
-          <div className="work-container">
-            <div className="work-canvas">
-              <Canvas>
-                
-              </Canvas>
+      <div className="w-full text-white-600">
+        <h3 className="head-text" > My work experience</h3>
+        <div className="work-container">
+          <div className="work-canvas">
 
+            <div className="work-content">
+              <div className="sm:py-10 sm:px-5 px-2.5">
+                {workExperiences.map(({ id, name, icon, pos, duration, title, animation }) => (
+                  <div key={id} className="work-content_container group">
+                    <div className="flex flex-col h-full justify-start items-center py-2">
+                      <div className="work-content_logo">
+                        <img src={icon} alt="logo" />
+                      </div>
+                      <div className="work-content_bar" />
+                    </div>
+                    <div>
+                      
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-
           </div>
-
         </div>
+      </div>
     </div>
   )
 }
