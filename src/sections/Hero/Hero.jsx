81d6c3cf843cd0e7801fa7c0f116ede5d34823f6
Léo Jansen <leo@leonardojansen.com>
2024-10-02 17:33:02 -0300

    Updates Hero

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,19 +1,35 @@
-import { Vector3 } from 'three'
-import { useRef } from 'react'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { useGLTF, SpotLight, useDepthBuffer, OrbitControls } from '@react-three/drei'
-import Statue from '../../components/Statue'
+import { Vector3 } from 'three';
+import { useEffect, useRef, useState } from 'react';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { useGLTF, SpotLight, SpotLightShadow, useDepthBuffer, OrbitControls, PerspectiveCamera, MeshReflectorMaterial, Environment, Backdrop } from '@react-three/drei';
+import Statue from '../../components/Statue';
+import HeroCamera from './components/HeroCamera';
+
+
 
 export default function Hero() {
+  const cameraRef = useRef();
+
+
+
+
+
   return (
-    <section className='h-screen'>
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [-2, 2, 16], fov: 50, near: 1, far: 230 }}>
-      <OrbitControls/>
-      <color attach="background" args={['#202020']} />
-      <fog attach="fog" args={['#202020', 5, 20]} />
-      <ambientLight intensity={0.543210951391015} />
-      <Scene />
-    </Canvas>
+    <section id="hero" className='h-screen w-full'>
+      <div className="absolute h-screen w-full -z-10">
+        <Canvas shadows dpr={[1, 2]} className="w-full">
+          <HeroCamera cameraRef={cameraRef} />
+          <color attach="background" args={['#000000']} />
+          <ambientLight intensity={0.32} />
+          <Scene />
+        </Canvas>
+      </div>
+      <div className="flex w-full h-full bg-transparent justify-center items-center">
+      <h1 className="text-white text-3xl ">Inspiration</h1>
+
+      </div>
+      
+
     </section>
   )
 }
@@ -22,17 +38,18 @@ function Scene() {
   // This is a super cheap depth buffer that only renders once (frames: 1 is optional!), which works well for static scenes
   // Spots can optionally use that for realism, learn about soft particles here: http://john-chapman-graphics.blogspot.com/2013/01/good-enough-volumetrics-for-spotlights.html
   const depthBuffer = useDepthBuffer({ frames: 1 })
-  const { nodes, materials } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/dragon/model.gltf')
+  const targetRef = useRef();
+
   return (
     <>
-      <MovingSpot depthBuffer={depthBuffer} color="#0c8cbf" position={[-3, 5, 3]} />
-      <MovingSpot depthBuffer={depthBuffer} color="#b00c3f" position={[-1, 5, 3]} />
-
-      <Statue receiveShadow castShadow scale={0.125}  position={[-5, -1,3]}/>
-      <mesh receiveShadow position={[0, -6, 10]} rotation-x={-Math.PI / 2}>
-        <planeGeometry args={[50, 50]} />
-        <meshPhongMaterial />
-      </mesh>
+      <MovingSpot depthBuffer={depthBuffer} color="rgb(222,255,255)" position={[1, 5, 3]} targetPosition={[0, 0, 0]} />
+      <Statue receiveShadow castShadow scale={0.125} position={[0, 0, 0]} />
+      <group ref={targetRef}>
+        <mesh receiveShadow position={[0, -4.867, 0]} rotation-x={-Math.PI / 2}>
+          <planeGeometry args={[50, 50]} />
+          <meshPhongMaterial color="rgb(80,10,80)" />
+        </mesh>
+      </group>
     </>
   )
 }
@@ -41,8 +58,8 @@ function MovingSpot({ vec = new Vector3(), ...props }) {
   const light = useRef()
   const viewport = useThree((state) => state.viewport)
   useFrame((state) => {
-    light.current.target.position.lerp(vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0), 0.1)
+    light.current.target.position.lerp(vec.set(2, -3, -5), 0.1)
     light.current.target.updateMatrixWorld()
   })
-  return <SpotLight castShadow ref={light} penumbra={1} distance={16} angle={2.1435} attenuation={17} anglePower={26} intensity={29} {...props} />
+  return <SpotLight castShadow ref={light} penumbra={111} distance={26} angle={0.85} attenuation={29} anglePower={21} intensity={222.8} {...props} />
 }
