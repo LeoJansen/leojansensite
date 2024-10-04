93bf8a1e4e0bb8eeecc827d4879e0be60ab0687a
Léo Jansen <leo@leoo.app>
2024-10-04 00:46:17 -0300

    Updates Canvas

--- src/sections/Hero/components/CanvasComponent.jsx
+++ src/sections/Hero/components/CanvasComponent.jsx
@@ -19,15 +19,15 @@ export const CanvasComponent = () => {
       <Suspense fallback={null}>
         <color attach="background" args={['black']} />
         <fog attach="fog" args={['black', 15, 20]} />
-        <ambientLight intensity={0.915} />
+        <ambientLight intensity={0.2915} />
         <spotLight position={[0, 10, 0]} intensity={0.3} />
         <directionalLight position={[-50, 0, -40]} intensity={0.17} />
 
         <group ref={ref}>
-          <Chair scale={0.01} position={[-3, 0, 0]} rotation={[0, 1, 0]} />
-          <Avatar animation={"sitting"} position={[-3,0,0]} rotation={[0, 1, 0]} />
+          <Chair scale={0.01} position={[-3, -1, 0]} rotation={[0, 1, 0]} />
+          <Avatar animation={"sitting"} position={[-3,-1,0]} rotation={[0, 1, 0]} />
           <Ground />
-          <VideoText position={[0, 1.3, -2]} />
+          <VideoText position={[0, 0.15, -2]} />
 
 
         </group>
