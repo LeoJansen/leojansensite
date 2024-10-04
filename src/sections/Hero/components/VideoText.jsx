bc7feac3efcf9a8dd68daad2f75d664540c6b3ab
Léo Jansen <leo@leoo.app>
2024-10-04 03:34:35 -0300

    Update VideoText.jsx

--- src/sections/Hero/components/VideoText.jsx
+++ src/sections/Hero/components/VideoText.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import * as THREE from "three";
 
 export function VideoText(props) {
-    const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/drei.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
+    const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/assets/heroVideo.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
     useEffect(() => void video.play(), [video])
     return (
       <Text font="/Inter-Bold.woff" fontSize={3} letterSpacing={-0.06} {...props}>
