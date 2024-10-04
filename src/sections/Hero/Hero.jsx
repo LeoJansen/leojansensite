ec5a568c35d0855c8dd09c6a43164e529d2d8ee9
Léo Jansen <leo@leoo.app>
2024-10-03 21:39:06 -0300

    Reorganizes Gero

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,6 @@
-import * as THREE from 'three'
-import React, { Suspense, useEffect, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei'
-import { Chair } from '../../../Chair';
 
-import { Avatar } from '../../components/Avatar';
+
+import { Canvas} from '@react-three/fiber'
 import { CanvasComponent } from './components/CanvasComponent';
 
 export default function Hero() {
@@ -13,15 +9,7 @@ export default function Hero() {
   return (
     <section className='w-full h-screen'>
     <Canvas concurrent gl={{ alpha: false }} pixelRatio={[1, 1.5]} camera={{ position: [0, 3, 100], fov: 15 }}>
-      <color attach="background" args={['black']} />
-      <fog attach="fog" args={['black', 15, 20]} />
-      <Suspense fallback={null}>
        <CanvasComponent />
-        <ambientLight intensity={0.15} />
-        <spotLight position={[0, 10, 0]} intensity={0.3} />
-        <directionalLight position={[-50, 0, -40]} intensity={0.17} />
-        <Intro />
-      </Suspense>
     </Canvas>
     </section>
   )
@@ -32,10 +20,4 @@ export default function Hero() {
 
 
 
-function Intro() {
-  const [vec] = useState(() => new THREE.Vector3())
-  return useFrame((state) => {
-    state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05)
-    state.camera.lookAt(0, 0, 0)
-  })
-}
+
--- src/sections/Hero/components/CanvasComponent.jsx
+++ src/sections/Hero/components/CanvasComponent.jsx
@@ -1,7 +1,7 @@
 
 import { useFrame } from "@react-three/fiber";
-
-import { useRef } from "react";
+import * as THREE from 'three'
+import { Suspense, useRef, useState } from "react";
 import { Chair } from "../../../../Chair";
 import { Avatar } from "../../../components/Avatar";
 import { Ground } from "./Ground";
@@ -22,18 +22,32 @@ export const CanvasComponent = () => {
 
   return (
     <>
+      <Suspense fallback={null}>
+      <color attach="background" args={['black']} />
+      <fog attach="fog" args={['black', 15, 20]} />
+        <ambientLight intensity={0.15} />
+        <spotLight position={[0, 10, 0]} intensity={0.3} />
+        <directionalLight position={[-50, 0, -40]} intensity={0.17} />
 
-      <group ref={ref}>
-        <Chair scale={0.01} position={[-3, 0, 0]} rotation={[0, 1, 0]} />
-        <Avatar animation="Typing" position={[-3, 0, 0]} rotation={[0, 1, 0]} />
-        <Ground />
-        <VideoText position={[0, 1.3, -2]} />
+        <group ref={ref}>
+          <Chair scale={0.01} position={[-3, 0, 0]} rotation={[0, 1, 0]} />
+          <Avatar animation="Typing" position={[-3, 0, 0]} rotation={[0, 1, 0]} />
+          <Ground />
+          <VideoText position={[0, 1.3, -2]} />
 
 
-      </group>
-
+        </group>
+        <Intro />
+      </Suspense>
     </>
   );
 };
 
 
+function Intro() {
+  const [vec] = useState(() => new THREE.Vector3())
+  return useFrame((state) => {
+    state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05)
+    state.camera.lookAt(0, 0, 0)
+  })
+}
