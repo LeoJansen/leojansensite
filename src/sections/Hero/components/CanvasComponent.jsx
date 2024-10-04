d42804536f80a9755db219fb0cfc43e10f258f75
Léo Jansen <leo@leoo.app>
2024-10-04 10:41:14 -0300

    Updates Hero

--- src/sections/Hero/components/CanvasComponent.jsx
+++ src/sections/Hero/components/CanvasComponent.jsx
@@ -19,15 +19,15 @@ export const CanvasComponent = () => {
       <Suspense fallback={null}>
         <color attach="background" args={['black']} />
         <fog attach="fog" args={['black', 15, 20]} />
-        <ambientLight intensity={0.2915} />
-        <spotLight position={[0, 10, 0]} intensity={0.3} />
-        <directionalLight position={[-50, 0, -40]} intensity={0.17} />
+        <ambientLight intensity={0.36} />
+        <spotLight position={[0, 10, -2]} intensity={0.96} />
+        <directionalLight position={[-50, 0, -40]} intensity={0.8317} />
 
         <group ref={ref}>
-          <Chair scale={0.01} position={[-3, -1, 0]} rotation={[0, 1, 0]} />
-          <Avatar animation={"sitting"} position={[-3,-1,0]} rotation={[0, 1, 0]} />
-          <Ground />
-          <VideoText position={[0, 0.15, -2]} />
+          <Chair scale={0.01} position={[-3, -2, -2]} rotation={[0, 1, 0]} />
+          <Avatar animation={"sitting"} position={[-3,-2,-2]} rotation={[0, 1, 0]} />
+          <Ground position={[0,-2,0]}/>
+          <VideoText />
 
 
         </group>
@@ -41,7 +41,7 @@ export const CanvasComponent = () => {
 function Intro() {
   const [vec] = useState(() => new THREE.Vector3())
   return useFrame((state) => {
-    state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05)
+    state.camera.position.lerp(vec.set(state.pointer.x * 5, 3 + state.pointer.y * 2, 13), 0.05)
     state.camera.lookAt(0, 0, 0)
   })
 }
