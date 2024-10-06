475fc424aca33c5ba90b758f3b3efc6bf2f57332
Léo Jansen <leo@leoo.app>
2024-10-05 22:18:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from "./sections/About";
 import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
 import WorkExperience from "./sections/Experience";
-
 import Footer from "./sections/Footer";
 import Hero from "./sections/Hero/Hero";
 import Navbar from "./sections/Navbar";
