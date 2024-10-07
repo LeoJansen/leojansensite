4c90321eeb209367ff843e90958e86cb875adee6
Léo Jansen <leo@leoo.app>
2024-10-07 18:45:09 -0300

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,6 +2,7 @@
 
 import { Canvas} from '@react-three/fiber'
 import { CanvasComponent } from './components/CanvasComponent';
+import { Button } from '../../components/Button';
 
 export default function Hero() {
 
@@ -11,6 +12,11 @@ export default function Hero() {
     <Canvas concurrent gl={{ alpha: false }} pixelRatio={[1, 1.5]} camera={{ position: [0, 3, 100], fov: 15 }}>
        <CanvasComponent />
     </Canvas>
+    <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+        <a href="#about" className="w-fit">
+          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+        </a>
+      </div>
     </section>
   )
 }
