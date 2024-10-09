476815d8bab341446c83e6ebf1dd91635e166544
Léo Jansen <leo@leonardojansen.com>
2024-10-09 18:43:28 -0300

    Update Projects.jsx

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -13,7 +13,7 @@ const projectCount = myProjects.length;
 const Projects = () => {
   const [selectedProjectIndex, setSelectedProjectIndex] = useState(0);
 
-  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isMobile = useMediaQuery({ maxWidth: 1024 });
 
 
 
@@ -40,7 +40,7 @@ const Projects = () => {
       <div className="flex w-full p-4">
         <p className="head-text ">My Work</p>
       </div>
-      <div className="flex flex-col w-full md:flex-row justify-center items-center ">
+      <div className="flex flex-col w-full lg:flex-row justify-center items-center ">
 
         <div className={`bg-black w-fit h-fit rounded-lg flex overflow-hidden`}>    
     
@@ -52,18 +52,13 @@ const Projects = () => {
 
 
         </div>
-        <div className="flex gap-5 mt-2 py-2 px-5 shadow-2xl w-[50vw] ">
-          <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
-            <div className="flex justify-between items-center">
+        <div className="flex gap-5 mt-2 py-2 px-5 shadow-2xl lg:w-[50vw] ">
+          <div className="grid grid-cols-4 relative gap-5 sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
+            <div className="flex w-full h-full justify-center items-center  col-start-1 row-start-1">
               <button className="arrow-btn"
                 onClick={() => handleNavigation('previous')}
               >
                 <img src="/assets/left-arrow.png" alt="left-arrow" className="w-4 h-4" />
-              </button>
-              <button className="arrow-btn"
-                onClick={() => handleNavigation('next')}
-              >
-                <img src="/assets/right-arrow.png" alt="right-arrow" className="w-4 h-4" />
               </button>
             </div>
 
@@ -72,7 +67,7 @@ const Projects = () => {
               <img src={currentProject.spotlight} alt="spotligth" className="w-full h-96 object-cover rounded-xl" />
             </div>
 
-            <div className="flex gap-4 items-center">
+            <div className="flex gap-4 items-center row-span-1 col-span-2 row-start-1 col-start-2">
               <div className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg " style={currentProject.logoStyle}>
                 <img src={currentProject.logo} alt="logo" className="w-10 h-10 shadow-sm" />
               </div>
@@ -80,12 +75,12 @@ const Projects = () => {
 
             </div>
 
-            <div className="flex flex-col gap-5 text-white-600 my-500">
+            <div className="flex flex-col gap-5 text-white-600 my-500 col-span-4 row-start-2">
 
               <p className="animatedText">{currentProject.desc}</p>
               <p className="animatedText">{currentProject.subdesc}</p>
             </div>
-            <div className="flex items-center justify-between flex-wrap gap-5">
+            <div className="flex flex-wrap gap-5 row-start-3 col-span-2 col-start-1 w-full h-full justify-center items-center ">
               <div className="flex items-center gap-3">
                 {currentProject.tags.map((tag, index) => (
                   <div key={index} className="tech-logo">
@@ -95,17 +90,28 @@ const Projects = () => {
 
               </div>
 
-              <a
-                className="flex items-center gap-2 cursor-pointer text-white-600"
+            
+            </div>
+            <div className="flex w-full h-full justify-center items-center col-span-2 col-start-3 row-start-3">
+            <a
+                className="flex w-full h-full justify-center items-center 3 gap-2 cursor-pointer text-white-600 "
                 href={currentProject.href}
                 target="_blank"
                 rel="noreferrer">
                 <p>Check Live Site</p>
                 <img src="/assets/arrow-up.png" alt="arrow" className="w-3 h-3" />
               </a>
+
             </div>
 
-
+            <div className="flex w-full h-full justify-center items-center col-start-4 row-start-1">
+  
+              <button className="arrow-btn"
+                onClick={() => handleNavigation('next')}
+              >
+                <img src="/assets/right-arrow.png" alt="right-arrow" className="w-4 h-4" />
+              </button>
+            </div>
 
 
 
