a0767dd980fa87c1636308cced1890dbaca5feec
Léo Jansen <leo@leonardojansen.com>
2024-10-10 18:53:38 -0300

    Update Navbar.jsx

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -27,18 +27,15 @@ const Navbar = () => {
     };
 
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 bg-black-300/30 px-auto backdrop-blur-sm">
-            <div className="max-w-7xl mx-auto">
-                <div className="flex justify-between items-center py-3 mx-auto">
+        <header className="fixed top-0 left-0 right-0 z-50 bg-black-300/30 backdrop-blur-sm">
+                <div className="flex justify-between items-center py-3 mx-12">
                    <img src="/logo.svg" alt="logo" className="w-[51px]" />
                     <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle Menu">
                         <img src={ isOpen ? 'assets/close.svg':'assets/menu.svg'} alt="menu" className="w-6 h-6"/>
                     </button>
                     <nav className="sm:flex hidden">
                         <NavItems/>
-
                     </nav>
-                </div>
             </div>
             <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
                 <nav className="p-5">
